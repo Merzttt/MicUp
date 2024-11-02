3f4ae021750e88a0115d2f86affdfe60351b173e
Merzt <120368875+Merzttt@users.noreply.github.com>
2024-11-02 20:33:16 +1100

    Create Platform.lua

---
+++ Platform.lua
@@ -0,0 +1,9 @@
+local part = Instance.new("Part", workspace)
+part.Material = Enum.Material.ForceField
+part.Color = Color3.fromRGB(255, 0, 255)
+part.CFrame = CFrame.new(0, -8.1, 0)
+part.Size = Vector3.new(5000, 16, 5000)
+part.Transparency = 0.5
+part.Anchored = true
+part.Name = "Federal's Platform"
+print("Platform by Federal")
